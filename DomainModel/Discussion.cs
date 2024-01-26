ad053f52ce1e2b302de849d78fb7b730632bfd6c
Logan Parker <lrparker2@bsu.edu>
2024-01-26 17:59:40 -0500

    Add discussion model to domain

---
+++ DomainModel/Discussion.cs
@@ -0,0 +1,26 @@
+namespace DomainModel;
+
+public class Discussions
+{
+    private string discussionName;
+
+    private string discussionPost;
+
+    private List<string> respopnsePosts;
+
+    private string submissionStatus;
+
+    private int dueDate;
+
+    private int pointsValue;
+
+    private int pointsEarned;
+
+    private int dateSubmitted;
+
+    private string timeSubmitted;
+
+    private void reply(string post) {
+        
+    }
+}
